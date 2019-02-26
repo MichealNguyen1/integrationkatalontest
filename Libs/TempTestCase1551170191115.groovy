43b3c3954cd12fc1ed0e9a5c9d898cd44ee4b787
Cuong <cuongnm.bk55@gmail.com>
2019-02-26 16:13:24 +0700

    Add files via upload

---
+++ Libs/TempTestCase1551170191115.groovy
@@ -0,0 +1,23 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import groovy.lang.MissingPropertyException
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('/var/folders/7_/1q8z908j5q73n7xb8snbzhbh0000gn/T/Katalon/Test Cases/Verify Correct Alarm Message/20190226_153630/execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/Verify Correct Alarm Message', new TestCaseBinding('Test Cases/Verify Correct Alarm Message',[:]), FailureHandling.STOP_ON_FAILURE , false)
+    
